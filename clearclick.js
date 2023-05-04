814975797468b93ec4c67cdcaa525cf928925804
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-05-03 22:19:40 -0300

    mobile first clearclick

--- clearclick.js
+++ clearclick.js
@@ -1,7 +1,1 @@
-const navbarBurger = document.querySelector('.navbar-burger');
-const navbarLinks = document.querySelector('.navbar-links');
 
-navbarBurger.addEventListener('click', () => {
-  navbarBurger.classList.toggle('active');
-  navbarLinks.classList.toggle('active');
-});
