c8bcbd0a00c3ad8ca4c29d39c9aa3462f3d8acda
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-05-07 13:29:18 -0300

    cleartime

---
+++ script/cleartime.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function() {
+    var menu = document.querySelector("#burguer");
+    if (window.pageYOffset > 20) {
+      menu.classList.add("menu-hamburguer-scroll");
+    } else {
+      menu.classList.remove("menu-hamburguer-scroll");
+    }
+  });
+  
+  
